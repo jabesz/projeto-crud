756a07d89bc2f7e9511cda7a8c0920631a5d9ad1
jabes <jabesvieira4@gmail.com>
2025-02-24 11:20:02 -0300

    Criação dos arquivos model e controller

--- backend/produtos/src/main/java/br/com/projetocrud/produtos/controller/ProductController.java
+++ backend/produtos/src/main/java/br/com/projetocrud/produtos/controller/ProductController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
-public class TestController {
+@RequestMapping("/rota")
+public class ProductController {
 
   @GetMapping
-  public String hello() {
-    return "Hello world";
+  public String rota() {
+    return "API esta funcionando!";
   }
 }
